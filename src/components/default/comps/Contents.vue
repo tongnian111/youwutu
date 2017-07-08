91da7ada2315d123e23ccde3d33de197adb09ac0
DESKTOP-GBSF44M\tongnian <1169042312@qq.com>
2017-07-08 17:28:53 +0800

    xiu

--- src/components/default/comps/Contents.vue
+++ src/components/default/comps/Contents.vue
@@ -6,10 +6,19 @@
 				<div class="swiper-container">
 					<div class="swiper-wrapper">
 						<div class="swiper-slide">
-							<img src="/static/img/default_banner_1.jpg" />
+							<a>
+								<img src="/static/img/default_banner_1.png" />
+							</a>
 						</div>
 						<div class="swiper-slide">
-							<img src="/static/img/default_banner_2.jpg" />
+							<a>
+								<img src="/static/img/default_banner_2.jpg" />
+							</a>
+						</div>
+						<div class="swiper-slide">
+							<a>
+								<img src="/static/img/default_banner_3.jpg" />
+							</a>
 						</div>
 					</div>
 					<div class="swiper-pagination"></div>
@@ -18,53 +27,58 @@
 			<ul class="listNav">
 				<li>
 					<a href="">
-						<img src="/static/img/huwai.png"/>
+						<img src="/static/img/huwai.png" />
 						<span>户外</span>
 					</a>
 				</li>
 				<li>
 					<a href="">
-						<img src="/static/img/xiangbao.png"/>
+						<img src="/static/img/xiangbao.png" />
 						<span>箱包</span>
 					</a>
 				</li>
 				<li>
 					<a href="">
-						<img src="/static/img/fuzhuang.png"/>
+						<img src="/static/img/fuzhuang.png" />
 						<span>服装</span>
 					</a>
 				</li>
 				<li>
 					<a href="">
-						<img src="/static/img/jiadian.png"/>
+						<img src="/static/img/jiadian.png" />
 						<span>家电</span>
 					</a>
 				</li>
 				<li>
 					<a href="">
-						<img src="/static/img/diannao.png"/>
+						<img src="/static/img/diannao.png" />
 						<span>电脑</span>
 					</a>
 				</li>
 				<li>
 					<a href="">
-						<img src="/static/img/shuma.png"/>
+						<img src="/static/img/shuma.png" />
 						<span>数码</span>
 					</a>
 				</li>
 				<li>
 					<a href="">
-						<img src="/static/img/shouji.png"/>
+						<img src="/static/img/shouji.png" />
 						<span>手机</span>
 					</a>
 				</li>
 				<li>
 					<a href="">
-						<img src="/static/img/quanbu.png"/>
+						<img src="/static/img/quanbu.png" />
 						<span>全部</span>
 					</a>
 				</li>
 			</ul>
+			<div class="banner">
+				<a>
+					<img src="/static/img/default_banner_1.png" />
+				</a>
+			</div>
 		</mt-loadmore>
 	</div>
 
@@ -125,37 +139,57 @@
 	#contents {
 		flex: 1;
 		overflow-y: auto;
-		#swiper{
+		#swiper {
 			height: R(322px);
 			width: 100%;
-			.swiper-container{
+			.swiper-container {
 				width: 100%;
 				height: 100%;
+				a {
+					display: block;
+					width: 100%;
+					height: 100%;
+					img {
+						width: 100%;
+					}
+				}
 			}
 		}
-		.listNav{
+		.listNav {
 			display: flex;
 			width: 100%;
 			flex-wrap: wrap;
 			margin-top: R(20px);
-			li{
+			li {
 				width: 25%;
 				height: R(135px);
 				text-align: center;
 				display: flex;
 				justify-content: center;
 				margin-bottom: R(24px);
-				a{
+				a {
 					width: R(102px);
 					height: 100%;
 					display: flex;
 					flex-direction: column;
-					img{
+					img {
 						width: 100%;
 					}
-					span{
+					span {
 						font-size: R(30px);
 					}
+				}
+			}
+		}
+		.banner {
+			width: 100%;
+			height: R(362px);
+			a {
+				display: block;
+				width: 100%;
+				height: 100%;
+				img {
+					width: 100%;
 				}
 			}
 		}
