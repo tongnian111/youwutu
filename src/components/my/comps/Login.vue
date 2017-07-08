1b342c1462c8f8aa9f65bacb85b3bdbe082a775d
zzhewd <544785380@qq.com>
2017-07-08 17:19:32 +0800

    张哲

--- src/components/my/comps/Login.vue
+++ src/components/my/comps/Login.vue
@@ -5,8 +5,11 @@
        <input type="text" placeholder="请输入11位手机号"> 
       </div>
       <div class="mi">
-        <p>密码</p>
+        密码
         <input type="password" placeholder="请输入6~20位的密码">
+        <p><a href="#">忘记密码</a></p>
+        <a href="#" class="denglu">登陆</a>
+        <a href="#/my/register" class="zhuce">注册</a>
       </div>
   </div>
 </template>
@@ -45,6 +48,56 @@ $ui-width:720px;
     background: #fff;
     font-size: R(26px);
     padding-left: R(36px);
+    margin-bottom: R(32px);
+    input{
+      outline: none;
+      border: 0;
+      border-left:R(4px) solid #ccc;
+      /*height: R(81px);
+      line-height: R(81px);*/
+      margin-left: R(34px);
+      width: R(500px);
+    }
+  }
+  .mi{
+    height: R(81px);
+    line-height: R(81px);
+    background: #fff;
+    font-size: R(26px);
+    padding-left: R(36px);
+    input{
+      outline: none;
+      border: 0;
+      margin-left: R(34px);
+      width: R(500px);
+    }
+  }
+  p{
+    height: R(58px);
+    line-height: R(58px);
+    margin-bottom: R(72px);
+    a{
+      text-decoration: underline;
+      font-size: R(26px);
+    float: right;
+    }
+  }
+  a.denglu{
+    display: block;
+    height: R(79px);
+    line-height: r(79px);
+    border: R(2px) solid #fc6e51;
+    color: #fc6e51;
+    text-align: center;
+    margin-bottom: R(31px);
+  }
+  a.zhuce{
+    display: block;
+    height: R(79px);
+    line-height: r(79px);
+    border: R(2px) solid #48cfad;
+    color: #48cfad;
+    text-align: center;
   }
 }
 </style>
