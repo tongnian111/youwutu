1d17905bef7501af2ad6c596b4751762cb7a0df4
zzhewd <544785380@qq.com>
2017-07-10 18:28:31 +0800

    no message

--- src/components/life/comps/Contents.vue
+++ src/components/life/comps/Contents.vue
@@ -1,6 +1,34 @@
 <template>
   <div id="contents">
-   		生活页面
+   		<div class="shangd">
+          <ul id="yipai">
+            <li @click="tar($event)">推荐</li>
+            <li @click="tar($event)">美景</li>
+            <li @click="tar($event)">美食</li>
+            <li @click="tar($event)">视频</li>
+            <li @click="tar($event)">发布</li>
+          </ul>   
+      </div>
+      <div class="swiper-container">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <a>
+                <img src="/static/img/my_ban_1.jpg" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a>
+                <img src="/static/img/my_ban_2.jpg" />
+              </a>
+            </div>
+            <div class="swiper-slide">
+              <a>
+                <img src="/static/img/my_ban_3.jpg" />
+              </a>
+            </div>
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
   </div>
 </template>
 
@@ -13,13 +41,32 @@ export default {
     }
   },
   methods:{
-  	
+  	tar:function(e){
+      // var a = e.target;
+      console.log(e.target)
+      
+      var yipai = document.getElementById("yipai").getElementsByTagName("li");
+      // console.log(yipai)
+      var len = yipai.length;
+      for(var i=0;i<len;i++){
+        console.log(target[i])
+        // len[i].style.color = "#000"
+      }
+      // console.log(lag)
+      e.target.style.color="#fc6e51";
+      // a.style.border="R(2px) solid #fc6e51";
+    }
   },beforeMount:function(){
   	//Dom加载前自动调用
   
   },mounted:function(){
   	//Dom加载完成自动调用此方法
-  
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: 3000, //可选选项，自动滑动
+        loop: true,
+        autoplayDisableOnInteraction: false,
+        pagination: '.swiper-pagination',
+      })
   }
 }
 </script>
@@ -33,5 +80,38 @@ $ui-width:720px;
 #contents{
 	flex: 1;
 	overflow-y: auto;
+  .shangd{
+    height:R(82px);
+    line-height: R(76px);
+    /*background: red;*/
+    ul{
+      display: flex;
+      height:R(82px);
+      line-height: R(76px);
+      justify-content: space-around;
+      li{
+        font-size: R(24px);
+        padding: 0 R(25px);
+        /*color: #fc6e51;
+        border-bottom: R(2px) solid #fc6e51;*/
+      }
+      li:hover{
+        color: #fc6e51;
+        border-bottom: R(2px) solid #fc6e51;
+      }
+    }
+  }
+  .swiper-container {
+        width: 100%;
+        height: 100%;
+        a {
+          display: block;
+          width: 100%;
+          height: 100%;
+          img {
+            width: 100%;
+          }
+        }
+      }
 }
 </style>
