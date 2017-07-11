d678fee28ab0c8af8a8ff5043a5a58ee53a7d0be
DESKTOP-GBSF44M\tongnian <1169042312@qq.com>
2017-07-11 16:59:34 +0800

    no message

--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-export const actions = {
-	//这里相当于react的 dispatch
-	footerNavIndex: ({ commit },{navIndex}) =>{
-		commit('footerNavIndex',{navIndex})
-	},
-	headerInfo:({ commit },{data}) =>{
-		commit('headerInfo',{data})
-	}
-}
