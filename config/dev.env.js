9379488d4bc0a780c1a6f06f4b5b19e4b6d720cf
zzhewd <544785380@qq.com>
2017-07-12 13:02:50 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,13 +4,8 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   proxyTable: {
-<<<<<<< HEAD
-  	'/youwutu/': {target: 'http://localhost/youwutuphp', changeOrigin: true},
-  	'/rightbe/api/': {target: 'http://www.daydayrise.net', changeOrigin: true},
-  	'/api/': {target: 'http://localhost/h51701/public', changeOrigin: true}
-=======
   	'/youwutu/': {target: 'http://10.2.153.97/youwutuphp', changeOrigin: true},
-  	'/rightbe/api/': {target: 'http://www.daydayrise.net', changeOrigin: true}
->>>>>>> b884d8f25bb7767111f55a6ee8463ab4173702f3
+  	'/rightbe/api/': {target: 'http://www.daydayrise.net', changeOrigin: true},
+  	'/api/': {target: 'http://10.2.153.227/h51701/public', changeOrigin: true}
   }
 })
