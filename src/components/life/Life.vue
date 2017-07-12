b86cdada915c4feb358d812b4dc8283c7b326066
DESKTOP-GBSF44M\tongnian <1169042312@qq.com>
2017-07-12 13:14:19 +0800

    no message

--- src/components/life/Life.vue
+++ src/components/life/Life.vue
@@ -31,21 +31,20 @@
 						</div>
 					</div>
 				</div>
-				<div class="kuang" v-for="item in list">
+				<div class="kuang" v-for="(item,index) in list">
 					<div class="ss">
-						<img :src="item.imgurl1">
-						<p>{{item.p1}}</p>
-						<p>{{item.p2}}</p>
+						<img :src="userInfo[index].header" :data-uid="userInfo[index].userid">
+						<p>{{userInfo[index].nickname}}</p>
+						<p>{{userInfo[index].time}}</p>
 					</div>
 					<div class="xx">
-						<img :src="item.imgurl2">
-						<p>{{item.p3}}</p>
-						<p>{{item.p4}}</p>
-						<p>...</p>
+						<img :src="headInfo[index].surl">
+						<p>{{headInfo[index].title}}</p>
+						<p class="content">{{item[0].content}}</p>
 						<ul class="xiakuang">
 							<li class="l">常识</li>
-							<li class="r"><i class="iconfont icon-jifen"></i>0</li>
-							<li class="r"><i class="iconfont icon-shoucangkong_"></i>2</li>
+							<li class="r"><i class="iconfont icon-jifen"></i>{{headInfo[index].ccount}}</li>
+							<li class="r"><i class="iconfont icon-shoucangkong_"></i>{{headInfo[index].lcount}}</li>
 							<li class="r"><i class="iconfont icon-xiaoxi1"></i>0</li>
 						</ul>
 					</div>
@@ -60,6 +59,7 @@
 	import { mapGetters, mapActions } from 'vuex';
 	import TopHeader from "../../components/public/TopHeader";
 	import FooterNav from "../../components/public/FooterNav";
+	import { Toast, Indicator } from "mint-ui";
 	//定义一个头部参数
 	let topArr = [{ //第一个参数
 		icon: "icon-search", //iconfont图标
@@ -87,22 +87,48 @@
 			return {
 				headerParams: topArr, //头部信息参数
 				activeIndex: 0,
+				headInfo: [],
+				userInfo: [],
 				list: []
 			}
 		},
 		methods: {
-			tar: function(e) {
-				this.activeIndex = e;
+			tar: function(index) {
+				this.activeIndex = index;
+				this.getData(index+1)
+			},
+			getData: function(page=1) {
+				Indicator.open({
+					text: '加载中...',
+					spinnerType: 'fading-circle'
+				});
+				var that = this;
+				this.$http.get('/youwutu/life/getLifeData',{params:{page:page}}).then(response => {
+					//					console.log(response);
+					let data = response.body.data;
+					let len = data.length;
+					that.headInfo.splice(0);
+					that.userInfo.splice(0);
+					that.list.splice(0);
+					for(var i = 0; i < len; i++) {
+						that.headInfo.push(data[i][0]);
+						that.userInfo.push(data[i][1]);
+						that.list.push(data[i].splice(3));
+					}
+//					console.log(that.list)
+					Indicator.close();
+					Toast({
+						message: response.body.message,
+						position: 'middle',
+						duration: 3000
+					});
+				}, response => {
+					Indicator.close();
+				});
 			}
 		},
 		beforeMount: function() { //Dom加载前自动调用
-			var that = this;
-			this.$http.get('/static/data/life_shuju.json').then(response => {
-				console.log(response)
-				that.list = response.body;
-			}, response => {
-				// error callback
-			});
+			this.getData();
 		},
 		mounted: function() {
 			//Dom加载完成自动调用此方法
@@ -117,114 +143,115 @@
 
 <style scoped lang="scss">
 	@charset "utf-8";
-$ui-width:720px;
-@function R($px) {
-    @return $px/$ui-width*7.2rem;
-}
-#contents{
-	flex: 1;
-	overflow-y: auto;
-  display: flex;
-  flex-direction: column;
-  .shangd{
-    width: 100%;
-    height:R(82px);
-    line-height: R(76px);
-    ul{
-      display: flex;
-      height:R(82px);
-      line-height: R(76px);
-      justify-content: space-around;
-      li{
-        font-size: R(24px);
-        padding: 0 R(25px);
-      }
-      li.active{
-        color: #fc6e51;
-        border-bottom: R(2px) solid #fc6e51;
-      }
-    }
-  }
-  .zhong{
-    flex: 1;
-    .swiper-container {
-        height: R(323px);
-        width: 100%;
-        a {
-          display: block;
-          width: 100%;
-          height: 100%;
-          img {
-            width: 100%;
-          }
-        }
-      }
-  .kuang{
-    height: R(790px);
-    padding:0 R(20px);
-    border-bottom: R(2px) solid #ccc;
-    .ss{
-      height: R(120px);
-      padding: R(20px) 0;
-      overflow: hidden;
-      img{
-        float: left;
-        height: R(101px);
-        margin-right: R(21px);
-      }
-      p:nth-of-type(1){
-        font-size: R(24px);
-        margin-top: R(14px);
-        margin-bottom: R(28px);
-      }
-      p:nth-of-type(2){
-        color: #798089;
-        font-size: R(18px);
-      }
-    }
-    .xx{
-      height: R(650px);
-      img{
-        height: R(339px);
-      }
-      p:nth-of-type(1){
-        margin-top: R(28px);
-        margin-bottom: R(41px);
-        font-size: R(24px);
-      }
-      p:nth-of-type(2){
-        font-size: R(20px);
-        color:#6c747e;
-        margin-bottom: R(41px);
-      }
-      p:nth-of-type(3){
-        color:#6c747e;
-        font-size: R(20px);
-        margin-bottom: R(10px);
-      }
-      ul.xiakuang{
-        height: R(42px);
-        font-size: R(20px);
-        li.l{
-          float: left;
-          color: #fc6e51;
-          height: R(38px);
-          line-height: R(38px);
-          width: R(76px);
-          text-align: center;
-          border:R(2px) solid #fc6e51;
-        }
-        li.r{
-          float: right;
-          margin-left: R(40px);
-          i{
-            margin-right: R(8px);
-          }
-        }
-      }
-    }
-  }
-  }
-  
-}
+	$ui-width:720px;
+	@function R($px) {
+		@return $px/$ui-width*7.2rem;
+	}
+	
+	#contents {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		.shangd {
+			width: 100%;
+			height: R(82px);
+			line-height: R(76px);
+			ul {
+				display: flex;
+				height: R(82px);
+				line-height: R(76px);
+				justify-content: space-around;
+				li {
+					font-size: R(24px);
+					padding: 0 R(25px);
+				}
+				li.active {
+					color: #fc6e51;
+					border-bottom: R(2px) solid #fc6e51;
+				}
+			}
+		}
+		.zhong {
+			flex: 1;
+			overflow-y: auto;
+			.swiper-container {
+				height: R(323px);
+				width: 100%;
+				a {
+					display: block;
+					width: 100%;
+					height: 100%;
+					img {
+						width: 100%;
+					}
+				}
+			}
+			.kuang {
+				height: R(790px);
+				padding: 0 R(20px);
+				border-bottom: R(2px) solid #ccc;
+				.ss {
+					height: R(120px);
+					padding: R(20px) 0;
+					overflow: hidden;
+					img {
+						float: left;
+						height: R(101px);
+						margin-right: R(21px);
+					}
+					p:nth-of-type(1) {
+						font-size: R(24px);
+						margin-top: R(14px);
+						margin-bottom: R(28px);
+					}
+					p:nth-of-type(2) {
+						color: #798089;
+						font-size: R(18px);
+					}
+				}
+				.xx {
+					height: R(650px);
+					img {
+						height: R(339px);
+					}
+					p:nth-of-type(1) {
+						margin-top: R(28px);
+						margin-bottom: R(41px);
+						font-size: R(24px);
+					}
+					p:nth-of-type(2) {
+						font-size: R(20px);
+						color: #6c747e;
+						margin-bottom: R(41px);
+					}
+					p.content{
+						display: -webkit-box;
+						-webkit-box-orient: vertical;
+						-webkit-line-clamp: 2; //文本行数
+						overflow: hidden;
+					}
+					ul.xiakuang {
+						height: R(42px);
+						font-size: R(20px);
+						li.l {
+							float: left;
+							color: #fc6e51;
+							height: R(38px);
+							line-height: R(38px);
+							width: R(76px);
+							text-align: center;
+							border: R(2px) solid #fc6e51;
+						}
+						li.r {
+							float: right;
+							margin-left: R(40px);
+							i {
+								margin-right: R(8px);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 </style>
