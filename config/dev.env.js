91fc0d79a63d407dc4d9978eb834d51566e6592f
DESKTOP-GBSF44M\tongnian <1169042312@qq.com>
2017-07-14 11:12:26 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,7 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   proxyTable: {
   	'/youwutuphp/youwutu/': {target: 'http://10.2.153.97', changeOrigin: true},
-  	'/rightbe/api/': {target: 'http://www.daydayrise.net', changeOrigin: true}
+  	'/rightbe/api/': {target: 'http://www.daydayrise.net', changeOrigin: true},
+  	'/youwutuphp/youwutu/': {target: 'http://localhost', changeOrigin: true}
   }
 })
